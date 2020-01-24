64242e65d183ba2ad08e3bbfc160561272b73b65
cmruss <cmrussell08@gmail.com>
2020-01-24 01:33:15 -0500

    Some stretch just to test

--- cookbook/cookbook-router.js
+++ cookbook/cookbook-router.js
@@ -4,4 +4,45 @@ const Cookbook = require('./cookbook-model');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    Cookbook.getRecipes()
+    .then(recipes => {
+        res.status(200).json(recipes);
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ errorMessage: 'Problem fetching recipes'})
+    });
+});
+
+router.get('/:id/shoppingList', (req, res) => {
+    Cookbook.getShoppingList(req.params.id)
+    .then(list => {
+        if (list) {
+            res.json(list);
+        } else {
+            res.status(404).json({ message: 'Recipe not found.'})
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ errorMessage: 'Failure connecting to database.'})
+    })
+});
+
+router.get('/:id/instructions', (req, res) => {
+    Cookbook.getInstructions(req.params.id)
+    .then(inst => {
+        if (inst) {
+            res.json(inst);
+        } else {
+            res.status(404).json({ message:'Instructions not found.' })
+        }
+    })
+    .catch(error => {
+        console.log(error)
+        res.status(500).json({ errorMessage: 'Failure connecting to database.'})
+    })
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const cookbookRouter = require('./cookbook/cookbook-router')
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+server.use('/api/recipes', cookbookRouter);
 
 
 module.exports = server;
